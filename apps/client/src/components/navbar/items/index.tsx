178f128a67b06a715a0c65506869e5639e79758a
BLACHERE Nicolas <nicolas.blachere@gmail.com>
2025-12-05 16:51:14 +0100

    style: amélioration style boutons navbar

--- apps/client/src/components/navbar/items/index.tsx
+++ apps/client/src/components/navbar/items/index.tsx
@@ -10,7 +10,7 @@ export interface ItemLinkProps {
 }
 
 export const ItemLink: React.FC<ItemLinkProps> = ({ item, className }) => (
-    <NavLink to={item.to ?? '#'} className={className ?? item.className ?? 'hover:text-primary transition-colors'}>
+    <NavLink to={item.to ?? '#'} className={className ?? item.className ?? 'hover:text-primary btn btn-ghost btn-neutral transition-colors'}>
         {item.title}
     </NavLink>
 );
